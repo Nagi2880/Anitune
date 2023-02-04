a2048a0a410ba5f2fd2ce96fee305b0298d1f537
Nagi2880 <alexanderamaral17@gmail.com>
2023-02-04 07:27:29 -0300

    starting the first desing for the home header

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Headerlist from '../elements/Headerlist';
-
+import styles from './components.module.css';
 export default function HeaderMain() {
   return (
-    <div>
-        <header>
-            <nav>
-              Anitune
-            </nav>
-            <Headerlist />
-        </header>
-    </div>
+    
+       <header id={styles.headerContainer} className="d-flex align-items-center justify-content-between" >       
+          <nav id={styles.navlogo}>
+            Anitune
+          </nav>
+          <Headerlist />
+      </header>
+
   )
 }
--- src/elements/Headerlist.tsx
+++ src/elements/Headerlist.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import styles from '../elements/elements.module.css';
 export default function Headerlist() {
   return (
-    <ul>
-        <li>Home</li>
-        <li>About us</li>
-        <button>Login</button>
-    </ul>
+    <nav id={styles.navbarbutton} className="d-flex  justify-content-center">
+      <button id={styles.butheader} className="">Home</button>
+      <button id={styles.butheader} className="">Songs</button>
+      <button id={styles.loginbutton} className="">Login</button>
+    </nav>
   )
 }
